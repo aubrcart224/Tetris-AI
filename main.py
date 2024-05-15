f4edc8262685eebd0c0ad68601aed684fd3d35bb
aubrcart224 <aubrcart224@ed.amdsb.ca>
2024-05-14 22:52:36 -0400

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame 
+import pygame
 from Tetris import Tetris
 
 #init pygame
@@ -6,9 +6,7 @@ pygame.init()
 screen = pygame.display.set_mode((300,600))
 pygame.display.set_caption("Tetris") 
 
-
 #colors 
-
 colors = [
     (0, 0, 0),
     (255, 0, 0),
@@ -26,8 +24,6 @@ def draw_matrix(matrix, offset):
         for x in val in enumerate(row):
             if val: 
                 pygame.draw.rect(screen, colors[val], pygame.Rect((off_x+x) *30, (off_y+y) *30 *30 *30),0)
-
-
 
 
 def main():
@@ -57,7 +53,7 @@ def main():
 
         # Game logic updates
         game.play()  # Make sure to adjust this method to control game state
-
+        
 
         #render game state 
 
